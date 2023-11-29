fa3ab3c00fd74a8388577d5b8d32110956f8c3c1
chasor <charlotte.sorensen@ki.se>
2023-11-29 12:18:11 +0100

    Loading and exploring of iris data

--- analysis.R/plotting iris data.R
+++ analysis.R/plotting iris data.R
@@ -1,12 +1,19 @@
 # Installation of packages
 #renv::install(c('tidyverse', 'ggpubr'))
 
-# Plots of data
+# Call packages
 library(tidyverse)
 library(ggpubr)
 
-# Plot Iris data
+# Snapshot with renv
+#renv::snapshot()
+
+# Loading and exploring iris data
+data(iris) 
 head(iris)
+summary(iris)
+str(iris)
+
 ggplot(iris,aes(x=Sepal.Length, y= Sepal.Width))+
   geom_point()+
   geom_smooth()
